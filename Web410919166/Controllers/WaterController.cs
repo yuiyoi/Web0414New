74036ef89e1bfbd8c7f2c076ab73529ea81ddf9f
徐羽萱 <s1091916@gm.pu.edu.tw>
2022-05-19 16:28:25 +0800

    補 喝水、註冊表單

---
+++ Web410919166/Controllers/WaterController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410919166.Controllers
+{
+    public class WaterController : Controller
+    {
+        // GET: Water
+        public ActionResult Index(String name, float? weight)
+        {
+            var result = "";
+            if (weight.HasValue)
+            {
+                result = weight * 30 + "cc";
+            }
+
+            ViewBag.yourName = name;
+            ViewBag.result = result;
+
+            return View();
+        }
+    }
+}
