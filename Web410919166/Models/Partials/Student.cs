d5051bd41f1ea9a42a0f048e953e02e2a88d4677
徐羽萱 <s1091916@gm.pu.edu.tw>
2022-06-09 20:14:08 +0800

    0609

--- Web410919166/Models/Partials/Student.cs
+++ Web410919166/Models/Partials/Student.cs
@@ -19,7 +19,7 @@ namespace Web410919166.Models
         [Required(ErrorMessage = "請填寫姓名")]
         [Display(Name = "姓名")]
         [StringLength(5, MinimumLength = 2, ErrorMessage = "請輸入2~5個字")]
-        public string Name { get; set; }
+        public string Username { get; set; }
 
         [Required(ErrorMessage = "請填寫學號")]
         [Display(Name = "學號")]
@@ -30,6 +30,10 @@ namespace Web410919166.Models
         [Display(Name = "電子郵件")]
         [EmailAddress]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "必填欄位")]
+        [Display(Name = "是否註冊")]
+        public bool IsActive { get; set; }
     }
 
 }
